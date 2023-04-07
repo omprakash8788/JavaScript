75bb8795e8a1a38cf9aae11140540f54e60fa41b
omprakash8788 <omprakashkumar552@gmail.com>
2023-04-07 12:22:09 +0530

    first commit

---
+++ Day1/script.js
@@ -0,0 +1,40 @@
+// variables , Constants and comment 
+
+//console.log("hi om");
+
+//var score =34 ;
+// = assign operator 
+
+// let point=23;
+// console.log(point);
+
+
+// constant 
+// const value =10;
+// console.log(value);
+
+
+//---------------- Data type and values -----
+// total we have 7 data type 
+
+// String  like "om", 'om'
+// Boolean true/false
+// Null - Intentionally absence of value
+// Undefined 
+// object
+// symbol
+// 
+
+// let score=null
+// console.log(score);
+
+// Strings - 
+console.log("Hello");
+let firstName="om";
+let lastName="kumar"
+console.log(firstName,lastName);
+
+// String concatination 
+
+
+
